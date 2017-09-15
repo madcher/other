9935cfb87295761fe1c7387afbed06a7e71a505c
Maxim <madcher@yandex.ru>
2017-09-15 17:53:17 +0300

    add status text

--- Reactable/src/App.js
+++ Reactable/src/App.js
@@ -16,11 +16,18 @@ class App extends Component {
       this.pageNumber = 1;
       this.start=0;
       this.end=50;
+      this.loading = "";
      }
 
+  loadingFunc(){
+    this.loading = "loading....";
+    console.log(this.loading);
+    this.setState({ lst: [] });
 
+  }
 
   loadData(num){
+    this.loadingFunc();
     let url = (num==1)? 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}':
     "http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";
     fetch(url)
@@ -29,8 +36,10 @@ class App extends Component {
     })
     .then((res) => {
     this.newstate = res;
+    this.loading = "";
     this.setState({ lst: res });
     });
+    console.log("sfsfsfafa2");
   }
 
   search(mass){
@@ -75,6 +84,7 @@ class App extends Component {
         <button onClick={ ()=> this.loadData("1")}>SMALL</button>
         <button onClick={()=> this.loadData("2")}>BIG</button>
         <Managedata items={this.state.lst.slice(0,50)} newitems={this.newstate} findData={this.search.bind(this)}/>
+        <div>{this.loading}</div>
         {navigate}
       </div>
     );
