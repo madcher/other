bb7469a1d5c15c811ab9c229fa1bec63952505b0
Maxim <madcher@yandex.ru>
2019-11-11 14:37:30 +0300

    Create data.js

---
+++ alec/data.js
@@ -0,0 +1,128 @@
+var massive = {
+ 	id1: {
+ 		name: 'company 1',
+ 		formName: '',
+ 		code: '',
+ 		index: '',
+ 		year: '',
+ 		version: '',
+ 		instruction: '',
+ 		folder: true,
+ 		childs: {
+ 			id555: {
+		 		name: 'company 1 child 1',
+		 		formName: 'xyz',
+		 		code: 11136,
+		 		index: 845614,
+		 		year: 2017,
+		 		version: 2,
+		 		instruction: 'what',
+		 		folder: false
+			},
+			id666: {
+		 		name: 'company 1 child 2',
+		 		formName: 'xyz',
+		 		code: 11136,
+		 		index: 845614,
+		 		year: 2017,
+		 		version: 2,
+		 		instruction: 'what',
+		 		folder: false
+			},
+			id676: {
+		 		name: 'XXX',
+		 		formName: '',
+		 		code: '',
+		 		index: '',
+		 		year: '',
+		 		version: '',
+		 		instruction: '',
+		 		folder: true,
+		 		childs: {
+		 			id434: {
+				 		name: 'hz.avi',
+				 		formName: 'xyz',
+				 		code: 11136,
+				 		index: 845614,
+				 		year: 2017,
+				 		version: 2,
+				 		instruction: 'what',
+				 		folder: false
+					},
+					id787: {
+				 		name: 'trash',
+				 		formName: 'xyz',
+				 		code: 11136,
+				 		index: 845614,
+				 		year: 2017,
+				 		version: 2,
+				 		instruction: 'what',
+				 		folder: false
+					}
+		 		}
+			}
+ 		}
+	},
+ 	id123: {
+ 		name: 'noname',
+ 		formName: 'xyz',
+ 		code: 11136,
+ 		index: 845614,
+ 		year: 2017,
+ 		version: 2,
+ 		instruction: 'what',
+ 		folder: false
+	},
+	id323: 	{
+ 		name: 'АО АУУУ',
+ 		formName: 'zxy',
+ 		code: 17156,
+ 		index: 845614,
+ 		year: 2019,
+ 		version: 10,
+ 		instruction: 'whaaaaaat',
+ 		folder: false
+	},
+	id143: 	{
+ 		name: 'Распилово',
+ 		formName: '',
+ 		code: '',
+ 		index: '',
+ 		year: '',
+ 		version: '',
+ 		instruction: '',
+ 		folder: true,
+ 		childs: {
+ 			id777: {
+		 		name: 'Распилово 2.0',
+		 		formName: 'xyz',
+		 		code: 11136,
+		 		index: 845614,
+		 		year: 2017,
+		 		version: 2,
+		 		instruction: 'what',
+		 		folder: false
+			},
+			id888: {
+		 		name: 'Растаскино',
+		 		formName: 'xyz',
+		 		code: 11136,
+		 		index: 845614,
+		 		year: 2017,
+		 		version: 2,
+		 		instruction: 'what',
+		 		folder: false
+			}
+ 		}
+	},
+	id153: 	{
+ 		name: 'OOO Шишки',
+ 		formName: 'xxx2',
+ 		code: 11156,
+ 		index: 845614,
+ 		year: 2017,
+ 		version: 0,
+ 		instruction: 'what',
+ 		folder: false
+	}
+}
